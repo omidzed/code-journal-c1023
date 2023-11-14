5ea4433bdffc754e4e99476d58877e2c696008c6
Omid Asadi <omiddzedd@gmail.com>
2023-11-14 20:27:15 +0000

    Adjusted code based on instructor guidance.

--- js/data.js
+++ js/data.js
@@ -1,18 +1,18 @@
 /* exported data */
 
-const data = {
+let data = {
   view: 'entry-form',
   entries: [],
   editing: null,
   nextEntryId: 1,
 };
-console.log(data);
+
 window.addEventListener('beforeunload', function (event) {
-  const entriesJSON = JSON.stringify(data.entries);
+  const entriesJSON = JSON.stringify(data);
   localStorage.setItem('javascript-local-storage', entriesJSON);
 });
 
 const previousEntriesJSON = localStorage.getItem('javascript-local-storage');
 if (previousEntriesJSON !== null) {
-  data.entries = JSON.parse(previousEntriesJSON);
+  data = JSON.parse(previousEntriesJSON);
 }
